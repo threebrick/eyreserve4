18e1e1d7840fc389097ba0e32f66e4a08b516549
threebrick <darnell@threebrick.com>
2017-02-28 21:17:54 -0500

    Updated new changes

--- app_old.js
+++ app_old.js
@@ -9,12 +9,15 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    //appId: process.env.MICROSOFT_APP_ID,
-    //appPassword: process.env.MICROSOFT_APP_PASSWORD
-    appId: '5778e28e-35e4-4cae-80eb-66d797996152',
-    appPassword: '6zh4NJX7zO5iFEuRdKJpXzq'
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    //appId: '5778e28e-35e4-4cae-80eb-66d797996152',
+    //appPassword: '6zh4NJX7zO5iFEuRdKJpXzq'
 });
 var bot = new builder.UniversalBot(connector);
+//var bot = new builder.UniversalBot(connector, function (session) {
+//    session.send("Hi... I'm the EY Reserve bot. I can help you reserve a seat or a conference room.  How may I help you?");
+//});
 server.post('https://eyreserve.azurewebsites.net/api/messages', connector.listen());
 
 // Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
@@ -32,11 +35,11 @@ var recognizer = new builder.LuisRecognizer(model);
 
 var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
 bot.dialog('/', dialog);
-
 dialog.onBegin(function (session, args, next) {
     session.send("Hi... I'm the EY Reserve bot. I can help you reserve a seat or a conference room.  How can I assist you do today?");
    // next();
 });
+
 
 // Add intent handlers
 dialog.matches('Reserve Seat', [
@@ -99,7 +102,7 @@ dialog.matches('Reserve Seat', [
         if (seatassignment.cityEntity && seatassignment.stateEntity && !seatassignment.buildingEntity) {
             builder.Prompts.text(session, 'What building?');
            //var style = builder.ListStyle.button;
-           // builder.Prompts.choice(session, "Which building?", "Building A|Building B|Building C", { listStyle: style });
+            //builder.Prompts.choice(session, "Got it.  There are 3 office buildings at that location.  Which building would you like?", "Building A|Building B|Building C", { listStyle: style });
         } else {
             next();
         }
@@ -109,14 +112,16 @@ dialog.matches('Reserve Seat', [
         if (results.response) {
             
             seatassignment.buildingEntity = results.response;
-        }
+        } 
+
+        
 
         
 
         // Prompt for time (title will be blank if the user said cancel)
         if (seatassignment.cityEntity && seatassignment.stateEntity && seatassignment.buildingEntity && !seatassignment.floorEntity) {
            // builder.Prompts.text(session, 'What building?');
-           builder.Prompts.text(session, 'What floor?');
+           builder.Prompts.text(session, 'And what floor?');
         } else {
             next();
         }
@@ -131,6 +136,8 @@ dialog.matches('Reserve Seat', [
         // Prompt for time (title will be blank if the user said cancel)
         if (seatassignment.cityEntity && seatassignment.stateEntity && seatassignment.buildingEntity && seatassignment.floorEntity && !seatassignment.seatEntity) {
             builder.Prompts.text(session, 'What seat?');
+            //var style = builder.ListStyle.button;
+            //builder.Prompts.choice(session, "We have 3 hot-seats available on the " + seatassignment.floorEntity + ". Please select the seat that you would like?", "Seat H101|Seat J205|Seat K102", { listStyle: style });
         } else {
             next();
         }
@@ -139,11 +146,11 @@ dialog.matches('Reserve Seat', [
         var seatassignment = session.dialogData.seatassignment;
         if (results.response) {
             seatassignment.seatEntity = results.response;
-        }
+        } 
 
         // Prompt for time (title will be blank if the user said cancel)
         if (seatassignment.cityEntity && seatassignment.stateEntity && seatassignment.buildingEntity && seatassignment.floorEntity && seatassignment.seatEntity && !seatassignment.dateEntity) {
-            builder.Prompts.text(session, 'What day?');
+            builder.Prompts.text(session, 'What day will you be needing this seat?');
         } else {
             next();
         }
@@ -156,7 +163,7 @@ dialog.matches('Reserve Seat', [
 
         // Prompt for time (title will be blank if the user said cancel)
         if (seatassignment.cityEntity && seatassignment.stateEntity && seatassignment.buildingEntity && seatassignment.floorEntity && seatassignment.seatEntity && seatassignment.dateEntity && !seatassignment.durationEntity) {
-            builder.Prompts.text(session, 'How long will you need this seat?');
+            builder.Prompts.text(session, 'Finally, how long would you like to reserve this seat?');
         } else {
             next();
         }
@@ -171,7 +178,7 @@ dialog.matches('Reserve Seat', [
         // Set the alarm (if title or timestamp is blank the user said cancel)
         if (seatassignment.cityEntity && seatassignment.stateEntity && seatassignment.buildingEntity && seatassignment.floorEntity && seatassignment.seatEntity && seatassignment.dateEntity && seatassignment.durationEntity) {
             
-            session.send('Ok, I will now reserve seat %s for you in %s, %s, at the %s on the %s on %s for %s',
+            builder.Prompts.text(session, 'Ok, would you like me to reserve seat %s for you in %s, %s, at the %s on the %s on %s for %s',
                 seatassignment.seatEntity,
                 seatassignment.cityEntity,
                 seatassignment.stateEntity,
@@ -180,158 +187,40 @@ dialog.matches('Reserve Seat', [
                 seatassignment.dateEntity,
                 seatassignment.durationEntity);
         } else {
-            session.send('I am sorry, but there was an issue with your reservation.');
+            session.send('Ok... no problem.');
         }
     }
 ]);
 
 dialog.matches('Reserve a conference room', [
     function (session, args, next) {
-        // Resolve and store any entities passed from LUIS.
+        // Resolve entities passed from LUIS.
+        var title;
+        var entity = builder.EntityRecognizer.findEntity(args.entities, 'builtin.alarm.title');
+        if (entity) {
+            // Verify its in our set of alarms.
+            title = builder.EntityRecognizer.findBestMatch(alarms, entity.entity);
+        }
         
-        var cityEntity = builder.EntityRecognizer.findEntity(args.entities, 'builtin.geography.city');
-        var stateEntity = builder.EntityRecognizer.findEntity(args.entities, 'builtin.geography.us_state');
-        var floorEntity = builder.EntityRecognizer.findEntity(args.entities, 'Floor');
-        var buildingEntity = builder.EntityRecognizer.findEntity(args.entities, 'Building');
-        var roomEntity = builder.EntityRecognizer.findEntity(args.entities, 'Room');
-        var durationEntity = builder.EntityRecognizer.findEntity(args.entities, 'Duration');
-        var dateEntity = builder.EntityRecognizer.findEntity(args.entities, 'builtin.datetime.date');
-
-        var roomassignment = session.dialogData.roomassignment = {
-          cityEntity: cityEntity ? cityEntity.entity : null,
-          stateEntity: stateEntity ? stateEntity.entity : null,
-          buildingEntity: buildingEntity ? buildingEntity.entity : null,
-          floorEntity: floorEntity ? floorEntity.entity : null,
-          roomEntity: roomEntity ? roomEntity.entity : null,
-          durationEntity: durationEntity ? durationEntity.entity : null,
-          dateEntity: dateEntity ? dateEntity.entity : null
-        };
-
- //       console.log('city - %s', cityEntity.entity);
- //       console.log('state - %s', stateEntity.entity);
-  //      console.log('building - %s', buildingEntity.entity);
-  //      console.log('floor - %s', floorEntity.entity);
-       // console.log('Seat = ' + seatEntity.entity);
-  //      console.log('Date = ' + dateEntity.entity);
-      //  console.log('Duration = ' + durationEntity.entity);
-        
-        // Prompt for title
-        if (!roomassignment.cityEntity) {
-            builder.Prompts.text(session, 'In which city would you like to reserve your room?');
+        // Prompt for alarm name
+        if (!title) {
+            builder.Prompts.choice(session, 'Which alarm would you like to delete?', alarms);
         } else {
-            next();
-        }
-    },
-    function (session, results, next) {
-        var roomassignment = session.dialogData.roomassignment;
-        if (results.response) {
-            roomassignment.cityEntity = results.response;
-        }
-
-        // Prompt for time (title will be blank if the user said cancel)
-        if (roomassignment.cityEntity && !roomassignment.stateEntity) {
-            builder.Prompts.text(session, 'Ok, and what state?');
-        } else {
-            next();
-        }
-    },
-    function (session, results, next) {
-        var roomassignment = session.dialogData.roomassignment;
-        if (results.response) {
-            roomassignment.stateEntity = results.response;
-        }
-
-        // Prompt for time (title will be blank if the user said cancel)
-        if (roomassignment.cityEntity && roomassignment.stateEntity && !roomassignment.buildingEntity) {
-            builder.Prompts.text(session, 'What building?');
-           //var style = builder.ListStyle.button;
-           // builder.Prompts.choice(session, "Which building?", "Building A|Building B|Building C", { listStyle: style });
-        } else {
-            next();
-        }
-    },
-    function (session, results, next) {
-        var roomassignment = session.dialogData.roomassignment;
-        if (results.response) {
-            
-            roomassignment.buildingEntity = results.response;
-        }
-
-        
-
-        // Prompt for time (title will be blank if the user said cancel)
-        if (roomassignment.cityEntity && roomassignment.stateEntity && roomassignment.buildingEntity && !roomassignment.floorEntity) {
-           // builder.Prompts.text(session, 'What building?');
-           builder.Prompts.text(session, 'What floor?');
-        } else {
-            next();
-        }
-    },
-    function (session, results, next) {
-        var roomassignment = session.dialogData.roomassignment;
-        if (results.response) {
-            
-            roomassignment.floorEntity = results.response;
-        }
-
-        // Prompt for time (title will be blank if the user said cancel)
-        if (roomassignment.cityEntity && roomassignment.stateEntity && roomassignment.buildingEntity && roomassignment.floorEntity && !roomassignment.roomEntity) {
-            builder.Prompts.text(session, 'What conference room?');
-        } else {
-            next();
-        }
-    },
-    function (session, results, next) {
-        var roomassignment = session.dialogData.roomassignment;
-        if (results.response) {
-            seatassignment.seatEntity = results.response;
-        }
-
-        // Prompt for time (title will be blank if the user said cancel)
-        if (roomassignment.cityEntity && roomassignment.stateEntity && roomassignment.buildingEntity && roomassignment.floorEntity && roomassignment.roomEntity && !roomassignment.dateEntity) {
-            builder.Prompts.text(session, 'What day?');
-        } else {
-            next();
-        }
-    },
-    function (session, results, next) {
-        var roomassignment = session.dialogData.roomassignment;
-        if (results.response) {
-            roomassignment.dateEntity = results.response;
-        }
-
-        // Prompt for time (title will be blank if the user said cancel)
-        if (roomassignment.cityEntity && roomassignment.stateEntity && roomassignment.buildingEntity && roomassignment.floorEntity && roomassignment.roomEntity && roomassignment.dateEntity && !roomassignment.durationEntity) {
-            builder.Prompts.text(session, 'How long will you need the room?');
-        } else {
-            next();
+            next({ response: title });
         }
     },
     function (session, results) {
-        var roomassignment = session.dialogData.roomassignment;
+        // If response is null the user canceled the task
         if (results.response) {
-            
-            roomassignment.durationEntity = results.response;
-        }
-        
-        // Set the alarm (if title or timestamp is blank the user said cancel)
-        if (roomassignment.cityEntity && roomassignment.stateEntity && roomassignment.buildingEntity && roomassignment.floorEntity && roomassignment.roomEntity && roomassignment.dateEntity && roomassignment.durationEntity) {
-            
-            session.send('Ok, I will now reserve room %s for you in %s, %s, at the %s on the %s on %s for %s',
-                roomassignment.roomEntity,
-                roomassignment.cityEntity,
-                roomassignment.stateEntity,
-                roomassignment.buildingEntity,
-                roomassignment.floorEntity,
-                roomassignment.dateEntity,
-                roomassignment.durationEntity);
+            delete alarms[results.response.entity];
+            session.send("Deleted the '%s' alarm.", results.response.entity);
         } else {
-            session.send('I am sorry, but there was an issue with your reservation.');
+            session.send('Ok... no problem.');
         }
     }
 ]);
 
-dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I can only handle seat & conference room reservations."));
+dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand. I can only help you reserve a seat or a conference room."));
 
 server.get('/', restify.serveStatic({
  directory: __dirname,
